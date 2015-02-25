6287e01743018a90442bd0b33ef63e1d7f404923
JeesooKim <jeesoo.joa@gmail.com>
2015-02-25 11:44:08 -0500

    Jeesoo

---
+++ Temaskaming/Controllers/FindadocController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Temiskaming.Controllers
+{
+    public class FindadocController : Controller
+    {
+        //
+        // GET: /Findadoc/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
