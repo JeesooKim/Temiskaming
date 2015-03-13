bc94a337502511e8d8a698e6e4322abae9245570
kaitlinhuynh <kaitlin.huynh92@gmail.com>
2015-03-13 16:51:27 -0400

    FAQ

---
+++ Temaskaming/Controllers/FAQController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using Temiskaming.Models;
+
+namespace Temiskaming.Controllers
+{
+    public class FAQController : Controller
+    {
+
+        FAQClass objFAQ = new FAQClass();
+
+
+
+
+        public ActionResult Index(string search)
+        {
+
+            var Fa = objFAQ.getFAQ();
+            return View();
+        }
+
+        public ActionResult Informations(int id)
+        {
+            var fa = objFAQ.getFAQ();
+            return View(fa);
+        }
+
+
+        [HttpPost]
+        public ActionResult insert(FAQ fa)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    objFAQ.commitInsert(fa);
+                    return RedirectToAction("FAQ");
+                }
+                catch
+                {
+                    return View();
+                }
+            }
+            return View();
+        }
+        public ActionResult update(int id)
+        {
+            var fa = objFAQ.getFAQByID(id);
+            if (fa == null)
+            {
+                return View("Not Found");
+            }
+            else
+            {
+                return View(fa);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult update(int id, FAQ fa)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    objFAQ.commitUpdate(id, fa.Question, fa.Answer);
+                    return RedirectToAction("Informations" + id);
+                }
+                catch
+                {
+                    return View();
+                }
+            }
+            return View();
+        }
+
+        public ActionResult delete(int id)
+        {
+            var fa = objFAQ.getFAQByID(id);
+            if (fa == null)
+            {
+                return View("Not Found");
+            }
+            else
+            {
+                return View(fa);
+            }
+        }
+        [HttpPost]
+        public ActionResult delete(int id, FAQ fa)
+        {
+            try
+            {
+                objFAQ.commitDelete(id);
+                return RedirectToAction("FAQ");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        public ActionResult NotFound()
+        {
+            return View();
+        }
+    }
+}
---
+++ Temaskaming/Models/FAQClass.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Temiskaming.Models
+{
+    public class FAQClass
+    {
+        //class name FAQClass
+        databaseDataContext objFAQ = new databaseDataContext();
+
+        public IQueryable<FAQ> getFAQ()
+        {
+            var allFAQ = objFAQ.FAQs.Select(x => x);
+            return allFAQ;
+        }
+
+
+        //get id(delete if not needed)
+        public FAQ getFAQByID(int _id)
+        {
+            var allFAQ = objFAQ.FAQs.SingleOrDefault(x => x.Id == _id);
+            return allFAQ;
+        }
+
+        //insert
+        public bool commitInsert(FAQ fa)
+        {
+            using (objFAQ) //displose of our connection
+            {
+                objFAQ.FAQs.InsertOnSubmit(fa);
+                objFAQ.SubmitChanges();
+                return true;
+            }
+
+        }
+
+        //update
+        public bool commitUpdate(int _id, string _question, string _answer)
+        {
+            using (objFAQ)
+            {
+                var FAQUpd = objFAQ.FAQs.Single(x => x.Id == _id);
+                FAQUpd.Question = _question;
+                FAQUpd.Answer = _answer;
+                objFAQ.SubmitChanges();
+                return true;
+            }
+
+        }
+
+        //Delete
+        public bool commitDelete(int _id)
+        {
+            using (objFAQ)
+            {
+                var FAQDel =
+                    objFAQ.FAQs.Single(x => x.Id == _id);
+                objFAQ.FAQs.DeleteOnSubmit(FAQDel);
+                objFAQ.SubmitChanges();
+                return true;
+            }
+        }
+
+
+
+    }
+}
