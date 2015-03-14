62ed83f4360cc6bc7b65ba987e9aa22037c46d2a
Sumeet Bhullar <sumeet.bhu@gmail.com>
2015-03-14 19:19:39 -0400

    Revert

--- Temaskaming/Controllers/NewsController.cs
+++ Temaskaming/Controllers/NewsController.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.EnterpriseServices.Internal;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using Temiskaming.Models;
 
 namespace Temiskaming.Controllers
 {
@@ -12,30 +10,11 @@ namespace Temiskaming.Controllers
     {
         //
         // GET: /News/
-        
+
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult newsadmin()
-        {
-            return View();
-        }
-
-        public ActionResult newsupdate()
-        {
-            return View();
-        }
-
-        public ActionResult newsdelete()
-        {
-            return View();
-        }
-
-        public ActionResult newsinsert()
-        {
-            return View();
-        }
     }
 }
