343fc3a2fbfc0521799301795e03e065196acdcb
borishaw <borishaw@gmail.com>
2015-03-16 17:21:39 -0400

    boris

--- Temaskaming/Controllers/NewsController.cs
+++ Temaskaming/Controllers/NewsController.cs
@@ -10,8 +10,7 @@ namespace Temiskaming.Controllers
 {
     public class NewsController : Controller
     {
-        //
-        // GET: /News/
+
         NewsClass objNews = new NewsClass();
         public ActionResult Index()
         {
@@ -25,19 +24,97 @@ namespace Temiskaming.Controllers
             return View(news);
         }
 
-        public ActionResult newsupdate()
+        public ActionResult newsupdate(int id)
         {
+            var news = objNews.GetNewsByID(id);
+            if (news == null)
+            {
+                return View("NotFound");
+            }
+            else
+            {
+                return View(news);
+            }
+            
+        }
+
+        [HttpPost]
+        public ActionResult newsupdate(int id, news news)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    objNews.NewsUpdate(id, news.title, news.content);
+                    return RedirectToAction("newsadmin");
+                }
+                catch
+                {
+                    return View();
+                }
+            }
             return View();
         }
 
-        public ActionResult newsdelete()
+        public ActionResult newsdelete(int id)
         {
-            return View();
+            var news = objNews.GetNewsByID(id);
+            if (news == null)
+            {
+                return View("NotFound");
+            }
+            else
+            {
+                return View(news);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult newsdelete(int id, news news)
+        {
+            try
+            {
+                objNews.NewsDelete(id);
+                return RedirectToAction("newsadmin");
+            }
+            catch
+            {
+                return View();
+            }
         }
 
         public ActionResult newsinsert()
         {
             return View();
         }
+
+        [HttpPost]
+        public ActionResult newsinsert(news news)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    objNews.NewsInsert(news);
+                    return RedirectToAction("newsadmin");
+                }
+
+                catch
+                {
+                    return View();
+                }
+            }
+
+            else
+            {
+                return View();
+            }
+        }
+
+        public ActionResult NotFound()
+        {
+            return View();
+        }
+
     }
 }
