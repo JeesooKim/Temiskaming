a6301120844c525c0e23b7f041b5a89fdde313d7
JeesooKim <jeesoo.joa@gmail.com>
2015-03-18 05:03:55 -0400

    JS-AdminUpdateMar18

--- Temaskaming/Controllers/DoctorsController.cs
+++ Temaskaming/Controllers/DoctorsController.cs
@@ -60,9 +60,7 @@ namespace FindADoctor.Controllers
             //AddRange: the method of the generic List collection to add all the distinct medical departments to the list. Without the Distinct modifier, duplicate departments would be added.
 
             ViewBag.medicalDepartment = new SelectList(DepartLst);
-
             
-
             switch (sortOrder)
             {
                 case "name_desc":
@@ -93,9 +91,8 @@ namespace FindADoctor.Controllers
             }
             return View(doctors);
         }// End of Index
-
-
-        ///
+        
+        
         public ActionResult Details(int id)
         {
             var doc = objDoc.getDoctorByID(id);
@@ -112,11 +109,90 @@ namespace FindADoctor.Controllers
         //*************** Below, action methods are only for Admin-side *****************//
         // Admin index page will be different and added below.
         // Insert, Update, and Delete
+       
+        public ActionResult FindADocAdmin(string sortOrder, string currentFilter, string medicalDepartment, string searchString, int? page)
+        {//Admin Index page:this action method is basically the same as the public index page 
+            //but three more options added, update and delete for each row (doctor), and insert
 
-        public ActionResult FindADocAdmin()
-        {//index page in the admin side
-            return View();
+            ViewBag.currentSort = sortOrder;
+            //"ViewBag.CurrentSort: keep the current sortorder the same while paging"
 
+            ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            ViewBag.DepartSortParm = sortOrder == "Department" ? "departmt_desc" : "Department";
+
+
+            ViewBag.currentFitler = searchString;
+            //"ViewBag.currentFilter : to maintain the filter setting during paging, with the textbox restoring the searchString Value when the page is redisplayed"
+
+            //string searchString =id;
+            var doctors = from d in objDoc.getDoctors()
+                          select d;
+            //the above lines are LINQ query to select the doctors, created by the IndexMethod
+            //The query is defined at this point, but hasn't been run against the data store.
+
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+            
+            var DepartLst = new List<string>();
+            //this creates a List object to hold medical departments from the database.
+            //the following code is a LINQ query that retrieves all the medical departments from the db.
+            var DepartQry = from m in objDoc.getDoctors()
+                            orderby m.department
+                            select m.department;
+            DepartLst.AddRange(DepartQry.Distinct());
+            //AddRange: the method of the generic List collection to add all the distinct medical departments to the list. Without the Distinct modifier, duplicate departments would be added.
+
+            ViewBag.medicalDepartment = new SelectList(DepartLst);
+
+
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    doctors = doctors.OrderByDescending(s => s.lname);
+                    break;
+                case "Department":
+                    doctors = doctors.OrderBy(s => s.department);
+                    break;
+                case "departmt_desc":
+                    doctors = doctors.OrderByDescending(s => s.department);
+                    break;
+                default:
+                    doctors = doctors.OrderBy(s => s.lname);
+                    break;
+            }
+
+            //If the searchString parameter contains a string, the doctors query is modified to filter on the value of the search string, using the following code.
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                doctors = doctors.Where(s => s.lname.Contains(searchString));
+            }
+
+            //The following code shows how to check the medicalDepartment parameter. If it's not empty, the code further contains the doctors query to limit the selected doctors to the specified medical department.
+            if (!string.IsNullOrEmpty(medicalDepartment))
+            {
+                doctors = doctors.Where(x => x.department == medicalDepartment);
+            }            
+            return View(doctors);
+        }
+
+        public ActionResult FindADocDetails(int id)
+        {
+            var doc = objDoc.getDoctorByID(id);
+            if (doc == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(doc);   //when get a doc's info having id, return it to Details page(View)
+            }
         }
 
         public ActionResult FindADocInsert()
