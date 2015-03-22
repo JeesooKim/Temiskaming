8877a7c1d7ce28cde107ccea77520399d8d247a3
Sumeet Bhullar <sumeet.bhu@gmail.com>
2015-03-22 19:22:54 -0400

    Chattychat

--- Temaskaming/Controllers/ChatController.cs
+++ Temaskaming/Controllers/ChatController.cs
@@ -13,7 +13,50 @@ namespace Temiskaming.Controllers
 
         public ActionResult Index()
         {
-            return View();
+            return PartialView();
+        }
+
+        public ActionResult Chat()
+        {
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpPost]
+        public ActionResult Chat(string email)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.Email = email;
+                return PartialView();
+            }
+            else
+            {
+                return Index();
+            }
+        }
+
+        public ActionResult Send()
+        {
+            return PartialView();
+        }
+
+        [HttpPost]
+        public ActionResult Send(string message)
+        {
+            if (ModelState.IsValid)
+            {
+                return PartialView();
+            }
+            else
+            {
+                return PartialView();
+            }
+            
+        }
+
+        public ActionResult Exit()
+        {
+            return RedirectToAction("Index");
         }
 
     }
