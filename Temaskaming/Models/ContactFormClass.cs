b50a3b0888f3142eee221db97c65f69b6c6179c8
kaitlinhuynh <kaitlin.huynh92@gmail.com>
2015-03-23 20:44:09 -0400

    contact form view pages

---
+++ Temaskaming/Models/ContactFormClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Temiskaming.Models
+{
+    public class ContactFormClass
+    {
+        databaseDataContext objCF = new databaseDataContext();
+
+   public IQueryable<ContactForm> getCF()
+        {
+
+        }
+
+    
+    
+    }
+
+   
+
+
+}
