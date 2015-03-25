1de80e7ec3b5b963a3a85f41e9e2f11936635e75
kaitlinhuynh <kaitlin.huynh92@gmail.com>
2015-03-25 10:00:46 -0400

    Revert "Contact Form - KH"
    This reverts commit 8e948ccc2d866793eb48be0a6ad8cb438810bc2a.

--- Temaskaming/Models/ContactFormClass.cs
+++ Temaskaming/Models/ContactFormClass.cs
@@ -7,7 +7,6 @@ namespace Temiskaming.Models
 {
     public class ContactFormClass
     {
-
         databaseDataContext objCF = new databaseDataContext();
 
    public IQueryable<ContactForm> getCF()
@@ -17,35 +16,8 @@ namespace Temiskaming.Models
         }
 
         //get id 
-   public ContactForm getCFByID(int _id)
-   {
-       var allCF = objCF.ContactForms.SingleOrDefault(x => x.id == _id);
-       return allCF; 
-   }
-
-        //insert for public view page
-        public bool commitInsert(ContactForm CF)
-   {
-            using (objCF)//displose of our connection
-            {
-                objCF.ContactForms.InsertOnSubmit(CF);
-                objCF.SubmitChanges();
-                return true; 
-            }
-   }
-   
-        //Delete for admin 
-        public bool commitDelete(int _id)
-        {
-            using (objCF)
-            {
-                var CFDel =
-                    objCF.ContactForms.Single(x => x.id == _id);
-                objCF.ContactForms.DeleteOnSubmit(CFDel);
-                objCF.SubmitChanges();
-                return true;
-            }
-        }
+         public ContactForm 
+    
     
     }
 
