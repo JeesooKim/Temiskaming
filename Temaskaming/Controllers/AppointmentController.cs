8c45fdffcc024063432ca39bb4d2caf1f20acc7b
Ashwin <alagappan.pal@gmail.com>
2015-03-25 11:32:29 -0400

    appoint added

---
+++ Temaskaming/Controllers/AppointmentController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Temiskaming.Controllers
+{
+    public class AppointmentController : Controller
+    {
+        //
+        // GET: /Appointment/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
