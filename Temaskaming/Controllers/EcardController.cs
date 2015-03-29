b2f485bad2e591e1b1566b446083e1c7a5f20856
NikaOrlova <orlova.nika@gmail.com>
2015-03-29 19:02:35 -0400

    email signup

---
+++ Temaskaming/Controllers/EcardController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Temiskaming.Models;
+
+// controller which creates new Ecard
+namespace VeronikaProject.Controllers
+{
+    public class EcardController : Controller
+    {
+        linqecardsClass objEcard = new linqecardsClass();
+
+        //action method which displays all images for Ecard to be selected
+        public ActionResult SelectImage()
+        {
+            var cardimage = objEcard.getEcardImages();
+            return View(cardimage);
+        }
+
+        //action method which shows a form to create Ecard
+        public ActionResult CreateEcard(string passurl)
+        {
+            @ViewBag.valurl = passurl;
+            return View();
+        }
+
+        //action method which redirects to the first page of creating Ecard if user gets final ecardsend page not through post method
+        public ActionResult CardSent()
+        {
+            return RedirectToAction("CreateEcard");
+        }
+
+        //action result which on post inserts new data into Ecard table is the data is valid
+        [HttpPost]
+        public ActionResult CardSent(ecard valid)
+        {
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    objEcard.commitInsert(valid);
+                    return PartialView(valid);
+                }
+                catch
+                {
+                    return View("CreateEcard");
+                }
+            }
+            else
+            {
+                return View("CreateEcard");
+            }
+        }
+    }
+}
