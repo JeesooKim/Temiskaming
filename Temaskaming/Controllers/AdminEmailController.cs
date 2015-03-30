2fbfd26c58a464e1ba3a4812b039ccbe64993864
NikaOrlova <orlova.nika@gmail.com>
2015-03-30 17:45:51 -0400

    AdminEmail

---
+++ Temaskaming/Controllers/AdminEmailController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Temiskaming.Models;
+namespace Temiskaming.Controllers
+{
+    // controller which inserts, updates, and deletes data email signup table
+
+    public class AdminEmailController : Controller
+    {
+        linqemailsClass objSignup = new linqemailsClass();
+
+        //action result which shows all email signups
+        public ActionResult AdminShowSignups()
+        {
+            var signups = objSignup.getSignups();
+            return View(signups);
+        }
+
+    }
+}
