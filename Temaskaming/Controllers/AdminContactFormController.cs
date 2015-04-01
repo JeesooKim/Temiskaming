0b0a34ab2773ef685785ce19f33e6f6faa5741f7
kaitlinhuynh <kaitlin.huynh92@gmail.com>
2015-04-01 11:07:48 -0400

    contactForm fix errors

--- Temaskaming/Controllers/AdminContactFormController.cs
+++ Temaskaming/Controllers/AdminContactFormController.cs
@@ -14,7 +14,16 @@ namespace Temiskaming.Controllers
 
         ContactFormClass objCF = new ContactFormClass();
 
-        public ActionResult CFAdmin()
+
+        public ActionResult ContactFormAdmin()
+        {
+            var Cf = objCF.getCF();
+            return View(Cf);
+        }
+
+
+        //show Detail of each one
+        public ActionResult ContactFormDetail()
         {
             ViewBag.Group = "Contact Form";
             var Cf = objCF.getCF();
@@ -28,9 +37,35 @@ namespace Temiskaming.Controllers
             }
         }
 
-        public ActionResult CFDelete()
+        //Delete
+        public ActionResult ContactFormDelete(int id)
         {
-            return View();
+            var Cf = objCF.getCFById(id);
+            if (Cf == null)
+            {
+                return View("CFAdmin");
+            }
+            else
+            {
+                return View(Cf);
+            }
         }
+        
+       //Delete
+        [HttpPost]
+        public ActionResult ContactFormDelete(int id, ContactForm Cf)
+        {
+            ViewBag.Group = "Contact Form";
+            try
+            {
+                objCF.CommitDelete(id);
+                return RedirectToAction("CFAdmin");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
     }
 }
