c68962ddb1fd0b2474c1e61b22aa02fdb90a66ae
Sumeet Bhullar <sumeet.bhu@gmail.com>
2015-04-03 18:52:23 -0400

    donations

---
+++ Temaskaming/Models/donationsDB.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Temiskaming.Models
+{
+    public class donationsDB
+    {
+        databaseDataContext objDon = new databaseDataContext();
+
+        public IQueryable<donation> getAllDonations()
+        {
+            var donations = objDon.donations.Select(x => x);
+            return donations;
+        }
+
+        public donation getDonationById(int _id)
+        {
+            var donation = objDon.donations.SingleOrDefault(x => x.id == _id);
+            return donation;
+        }
+    }
+}
