f85ed128edaf3e2330afafdd37b91c363f9245f8
Sumeet Bhullar <sumeet.bhu@gmail.com>
2015-04-05 18:28:02 -0400

    don ipn

--- Temaskaming/Controllers/DonationController.cs
+++ Temaskaming/Controllers/DonationController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Net;
+using System.Text;
+using System.IO;
 
 using Temiskaming.Models;
 
@@ -19,24 +22,11 @@ namespace Temiskaming.Controllers
             return View();
         }
 
-        public ActionResult DonationRev()
-        {
-            return Index();
-        }
-
-        [HttpPost]
         public ActionResult DonationRev(donationsPublic don)
         {
-            if (ModelState.IsValid)
-            {
-
-                return View(don);
-            }
-            else
-            {
-                return Index();
-            }
+            return View();
         }
+
         public ActionResult SubmitDonation()
         {
             return Index();
@@ -45,8 +35,65 @@ namespace Temiskaming.Controllers
         [HttpPost]
         public ActionResult SubmitDonation(donationsPublic don)
         {
-            objDon.makeDonation(don);
-            return RedirectToAction("Index", "Home");
+            if (ModelState.IsValid)
+            {
+                objDon.makeDonation(don);
+                var donation = objDon.getLatestDonation();
+                don.id = donation.id;
+                return View("DonationRev", don);
+            }
+            else
+            {
+                return Index();
+            }
+        }
+
+
+        
+        public ActionResult IPN()
+        {
+            var ipnVals = new Dictionary<string, string>();
+            ipnVals.Add("cmd", "_notify-validate");
+            string url = "https://www.sandbox.paypal.com/cgi-bin/webscr";
+
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
+
+            req.Method = "POST";
+            req.ContentType = "application/x-www-form-urlencoded";
+
+            byte[] param = Request.BinaryRead(Request.ContentLength);
+            string strReq = Encoding.UTF8.GetString(param);
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append(strReq);
+
+            foreach (string key in ipnVals.Keys)
+            {
+                sb.AppendFormat("&{0}={1}", key, ipnVals[key]);
+            }
+            strReq += sb.ToString();
+            req.ContentLength = strReq.Length;
+
+            string response = "";
+            using (StreamWriter streamOut = new StreamWriter(req.GetRequestStream(), Encoding.UTF8))
+            {
+                streamOut.Write(strReq);
+                streamOut.Close();
+                using (StreamReader streamIn = new StreamReader(req.GetResponse().GetResponseStream()))
+                {
+                    response = streamIn.ReadToEnd();
+                }
+            }
+
+            if (response == "VERIFIED")
+            {
+                string tranID = Request["txn_id"];
+                string amount = Request["mc_gross"];
+                string donation_id = Request["donation_id"];
+
+                //objDon.verifyDonation(Convert.ToInt32(donation_id));
+            }
+            return View();
         }
 
         public ActionResult Success()
--- Temaskaming/Models/donationsDB.cs
+++ Temaskaming/Models/donationsDB.cs
@@ -21,21 +21,24 @@ namespace Temiskaming.Models
             return donation;
         }
 
+        public donation getLatestDonation()
+        {
+            var donation = objDon.donations.OrderByDescending(x => x.id).First();
+            return donation;
+        }
+
         public bool makeDonation(donationsPublic donation)
         {
-            using (objDon)
-            {
-                donation don = new donation();
-                don.first_name = donation.fname;
-                don.last_name = donation.lname;
-                don.email = donation.email;
-                don.amount = donation.amount;
-                don.message = donation.message;
-                don.donation_date = DateTime.Now;
-                objDon.donations.InsertOnSubmit(don);
-                objDon.SubmitChanges();
-                return true;
-            }
+            donation don = new donation();
+            don.first_name = donation.fname;
+            don.last_name = donation.lname;
+            don.email = donation.email;
+            don.amount = donation.amount;
+            don.message = donation.message;
+            don.donation_date = DateTime.Now;
+            objDon.donations.InsertOnSubmit(don);
+            objDon.SubmitChanges();
+            return true;
         }
 
         public bool verifyDonation(int _id)
@@ -46,10 +49,6 @@ namespace Temiskaming.Models
                 if (donation.verify == false)
                 {
                     donation.verify = true;
-                }
-                else if (donation.verify == true)
-                {
-                    donation.verify = false;
                 }
                 objDon.SubmitChanges();
                 return true;
