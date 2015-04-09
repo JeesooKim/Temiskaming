2d81b2adaf6535b0ac875ab756c9cbf56f5ce49d
Ashwin <alagappan.pal@gmail.com>
2015-04-09 15:30:39 -0400

    poll model and controller

--- Temaskaming/Controllers/SurveyController.cs
+++ Temaskaming/Controllers/SurveyController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using Temiskaming.Models;
+
 namespace Temiskaming.Controllers
 {
     public class SurveyController : Controller
@@ -11,16 +13,128 @@ namespace Temiskaming.Controllers
         //
         // GET: /Survey/
 
+        // properties
+        surveyClass pollObj = new surveyClass();
+
+        //
+        // GET: /Alert/
+
         public ActionResult Index()
         {
-            return View();
+            ViewBag.Group = "Admin";
+            var allPolls = pollObj.getAllPolls();
+            if (allPolls == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(allPolls);
+            }
+
         }
 
-        public ActionResult AddSurvey()
+        public ActionResult PostedPoll()
         {
+            var onePoll = pollObj.getActivePoll();
+            if (onePoll == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(onePoll);
+            }
+
+        }
+
+        public ActionResult AddPoll()
+        {
+            ViewBag.Group = "Admin";
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddPoll(poll newPoll)
+        {
+            newPoll.published = false;
+            ViewBag.Group = "Admin";
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    pollObj.commitInsertPoll(newPoll);
+                    return RedirectToAction("Index");
+                }
+                catch
+                {
+                    return View();
+                }
+            }
 
             return View();
         }
 
+        public ActionResult UpdatePoll(int id)
+        {
+            ViewBag.Group = "Admin";
+            poll loadPoll = pollObj.getPollByID(id);
+            if (loadPoll == null)
+            {
+                return View();
+            }
+            else
+            {
+                return View(loadPoll);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult UpdatePoll(int id, poll updPoll)
+        {
+            ViewBag.Group = "Admin";
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    pollObj.commitUpdatePoll(id, updPoll.question, updPoll.option1, updPoll.option2);
+                    return RedirectToAction("Index");
+                }
+                catch
+                {
+                    return View();
+                }
+            }
+
+            return View();
+        }
+
+        public ActionResult DeletePoll()
+        {
+            ViewBag.Group = "Admin";
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult DeletePoll(int id)
+        {
+            ViewBag.Group = "Admin";
+            if (id > 0)
+            {
+                try
+                {
+                    pollObj.commitDeletePoll(id);
+                    return RedirectToAction("Index");
+                }
+                catch
+                {
+                    return View();
+                }
+            }
+
+            return View();
+        }
+
+       
     }
 }
--- Temaskaming/Models/surveyValidation.cs
+++ Temaskaming/Models/surveyValidation.cs
@@ -3,9 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Web.Mvc;
+
 namespace Temiskaming.Models
 {
+
+    [MetadataType(typeof(alertValidation))]
+    public partial class poll
+    {
+
+    }
+
+
+    [Bind(Exclude = "id")]
     public class surveyValidation
     {
+        
+        public int id;
+
+        public string question;
+
+        public string option1;
+
+        public string option2;
+
+        public int choice1;
+
+        public int choice2;
+
+        public bool published;
+    
     }
 }
