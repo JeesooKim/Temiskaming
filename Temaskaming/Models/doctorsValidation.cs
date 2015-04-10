9cefca9469c0e126ea0896ad8c9bb76f6e25ca9a
JeesooKim <jeesoo.joa@gmail.com>
2015-04-10 18:58:14 -0400

    commit to pull

--- Temaskaming/Models/doctorsValidation.cs
+++ Temaskaming/Models/doctorsValidation.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-using System.ComponentModel.DataAnnotations;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Web.Mvc;
 
 
