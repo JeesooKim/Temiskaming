0c69afa51367c8125234d3830b6e59c05265732b
kaitlinhuynh <kaitlin.huynh92@gmail.com>
2015-04-14 21:30:42 -0400

    validation

---
+++ Temaskaming/Models/OrderValidtion.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Web.Mvc;
+
+
+namespace Temiskaming.Models
+{
+    public class Ordervalidtion
+    {
+        public int OrderId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string FirstName { get; set; }
+        public string Lastname { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Province { get; set; }
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string ToPatient { get; set; }
+        public string From { get; set; }
+        public string Message { get; set; }
+
+    }
+}
