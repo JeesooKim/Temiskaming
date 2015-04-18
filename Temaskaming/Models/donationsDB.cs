82a4b96195b3903dc1c291078321bfcc768724a7
Sumeet Bhullar <sumeet.bhu@gmail.com>
2015-04-17 20:44:00 -0400

    comment donations

--- Temaskaming/Models/donationsDB.cs
+++ Temaskaming/Models/donationsDB.cs
@@ -11,30 +11,35 @@ namespace Temiskaming.Models
 
         public IQueryable<donation> getAllDonations()
         {
+            //Get all Donations
             var donations = objDon.donations.Select(x => x);
             return donations;
         }
 
         public donation getDonationById(int _id)
         {
+            //Get donation by ID
             var donation = objDon.donations.SingleOrDefault(x => x.id == _id);
             return donation;
         }
 
         public donation getLatestDonation()
         {
+            //Get latest donation
             var donation = objDon.donations.OrderByDescending(x => x.id).First();
             return donation;
         }
 
         public IQueryable<donation> getVerifiedDonations()
         {
+            //Get all verified donations
             var received = objDon.donations.Where(x => x.verify == true);
             return received;
         }
 
         public bool deleteAllUnverified()
         {
+            //Delete all unverified donations
             using (objDon)
             {
                 var unverified = objDon.donations.Where(x => x.verify == false && (DateTime.UtcNow - (DateTime)x.donation_date).TotalMinutes > 1);
@@ -46,6 +51,7 @@ namespace Temiskaming.Models
 
         public bool makeDonation(donationsPublic donation)
         {
+            //Make new donation
             donation don = new donation();
             don.first_name = donation.fname;
             don.last_name = donation.lname;
@@ -60,6 +66,7 @@ namespace Temiskaming.Models
 
         public bool verifyDonation(int _id)
         {
+            //IPN triggers this on confirmed donation
             using (objDon)
             {
                 var donation = objDon.donations.SingleOrDefault(x => x.id == _id);
@@ -74,6 +81,7 @@ namespace Temiskaming.Models
 
         public bool deleteDonation(int _id)
         {
+            //Delete a donation
             using (objDon)
             {
                 var donation = objDon.donations.SingleOrDefault(x => x.id == _id);
